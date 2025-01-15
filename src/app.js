32e3474a1f979d6238fff4dbec0a8ebc69b4a9a8
Rubenet8 <rubenferrermascarell@gmail.com>
2025-01-15 15:59:47 +0000

    Ejercicio terminado

--- src/app.js
+++ src/app.js
@@ -5,7 +5,38 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+function cardGenerate() {
+  let suits = ["♦", "♥ ", "♠", "♣"];
+  let numbers = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "K",
+    "R"
+  ];
+
+  let indexSuits = Math.floor(Math.random() * suits.length);
+  let indexNumbers = Math.floor(Math.random() * numbers.length);
+
+  document.querySelector("#top").innerHTML = suits[indexSuits];
+  document.querySelector("#button").innerHTML = suits[indexSuits];
+  document.querySelector("#number").innerHTML = numbers[indexNumbers];
+  if (suits[indexSuits] == "♦" || suits[indexSuits] == "♥") {
+    document.querySelector("#top").className = "text-danger";
+    document.querySelector("#button").className = "text-danger";
+  }
+}
+
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  cardGenerate();
 };
+
+setInterval(cardGenerate, 1000);
